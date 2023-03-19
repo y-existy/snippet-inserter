b0270e9e02179e04e16b920835b44487fed31647
existy <existy@existynoMacBook-Pro.local>
2023-03-19 21:23:06 +0900

    wip: add extension-filter feature

--- src/extension.ts
+++ src/extension.ts
@@ -14,4 +14,27 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   context.subscriptions.push(setFolderDisposable);
+
+  async function updateExtensions() {
+    const config = vscode.workspace.getConfiguration('snippet-inserter');
+    const currentExtensions = config.get('targetFileExtensions', ['.txt']);
+  
+    const userInput = await vscode.window.showInputBox({
+      prompt: 'Enter file extensions separated by commas (e.g., .txt, .md)',
+      value: currentExtensions.join(', '),
+    });
+  
+    if (userInput !== undefined) {
+      const newExtensions = userInput
+        .split(',')
+        .map((ext) => ext.trim())
+        .filter((ext) => ext.startsWith('.'));
+  
+      await config.update('targetFileExtensions', newExtensions, vscode.ConfigurationTarget.Global);
+    }
+  };
+  // Register the new command in the `activate` function
+  context.subscriptions.push(
+    vscode.commands.registerCommand('snippet-inserter.updateExtensions', updateExtensions)
+  );
 }
